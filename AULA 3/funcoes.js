a6bd6446cd50d7eda5dcf140fa89ad71c9c772a7
Alan Mareines <alanmareines@gmail.com>
2022-07-28 16:17:37 -0300

    fix: remove functions

--- AULA 3/funcoes.js
+++ AULA 3/funcoes.js
@@ -9,48 +9,40 @@ function primeiraFuncao() {
 
 function myGithubUsername() {
     // TODO: A função deve retornar o seu usuário Github
-    return 'alanmareines'
 }
 
 function tchauEspacosEmBranco(stringExemplo) {
     // TODO: A função deve retirar todos os espaços em branco da string que é fornecida como argumento
     // Exemplo: "     oi    " deve se tornar "oi"
-    return stringExemplo.trim()
 }
 
 function tudoMaiusculo(nomeAleatório) {
     // TODO: A função deve alterar todos os caractéres da string fornecida como argumento para maiúscula
     // Exemplo: "linaAcademy" deve se tornar "LINAACADEMY"
-    return nomeAleatório.toUpperCase()
 }
 
 function tudoMinusculo(nomeAleatório) {
     // TODO: Agora ao contrário - vamos alterar todas os caractéres para minúsculo
     // Exemplo: "EUTOEMOCIONADO" deve se tornar "eutoemocionado"
-    return nomeAleatório.toLowerCase()
 }
 
 function fazParte(frase, palavra) {
-    return frase.includes(palavra)
     // TODO: Retorne true, caso a palavra faça parte da frase
     // Exemplo: fazParte("Saudações Rubro Negras", "Rubro") => true
     // Explicando em PT-BR: Se a frase é "Saudações Rubro Negras", e a palavra for "Rubro", a função retorna true
 }
 
 function substituicao(string, charQueSeraSub, charSubstituto) {
-    return string.replace(charQueSeraSub, charSubstituto)
     // TODO: Retorne uma nova string, com caractéres substituidos: charQueSeraSub por charSubstituto
     // Exemplo: substituicao("Lina", "a", "o") => "Lino"
 }
 
 function multiplicacao(num1, num2) {
-    return num1 * num2
     // TODO: Função que multiplica dois números e volta o resultado d aoperação
     // Exemplo: multiplicacao(3,2) => 6
 }
 
 function papagaio(string, numRepeticao) {
-    return string.repeat(numRepeticao)
     // TODO: Retorna uma string que é repetida pela quantidade de vezes igual à numRepeticao
     // Exemplo: papagaio("Como estamos?", 3) => "Como estamos?Como estamos?Como estamos?"
 }
