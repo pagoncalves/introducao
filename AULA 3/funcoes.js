b6e9836c59510fe660cb8d42830efd8cbd44e039
Alan Mareines <alanmareines@gmail.com>
2022-08-09 18:58:22 -0300

    feature: respostas aula 3

--- AULA 3/funcoes.js
+++ AULA 3/funcoes.js
@@ -9,42 +9,50 @@ function primeiraFuncao() {
 
 function myGithubUsername() {
     // TODO: A função deve retornar o seu usuário Github
+    return 'alanmareines'
 }
 
 function tchauEspacosEmBranco(stringExemplo) {
     // TODO: A função deve retirar todos os espaços em branco da string que é fornecida como argumento
     // Exemplo: "     oi    " deve se tornar "oi"
+    return stringExemplo.trim()
 }
 
 function tudoMaiusculo(nomeAleatório) {
     // TODO: A função deve alterar todos os caractéres da string fornecida como argumento para maiúscula
     // Exemplo: "linaAcademy" deve se tornar "LINAACADEMY"
+    return nomeAleatório.toUpperCase()
 }
 
 function tudoMinusculo(nomeAleatório) {
     // TODO: Agora ao contrário - vamos alterar todas os caractéres para minúsculo
     // Exemplo: "EUTOEMOCIONADO" deve se tornar "eutoemocionado"
+    return nomeAleatório.toLowerCase()
 }
 
 function fazParte(frase, palavra) {
     // TODO: Retorne true, caso a palavra faça parte da frase
     // Exemplo: fazParte("Saudações Rubro Negras", "Rubro") => true
     // Explicando em PT-BR: Se a frase é "Saudações Rubro Negras", e a palavra for "Rubro", a função retorna true
+    return frase.includes(palavra)
 }
 
 function substituicao(string, charQueSeraSub, charSubstituto) {
     // TODO: Retorne uma nova string, com caractéres substituidos: charQueSeraSub por charSubstituto
     // Exemplo: substituicao("Lina", "a", "o") => "Lino"
+    return string.replaceAll(charQueSeraSub, charSubstituto)
 }
 
 function multiplicacao(num1, num2) {
     // TODO: Função que multiplica dois números e volta o resultado d aoperação
     // Exemplo: multiplicacao(3,2) => 6
+    return Math.imul(num1, num2)
 }
 
 function papagaio(string, numRepeticao) {
     // TODO: Retorna uma string que é repetida pela quantidade de vezes igual à numRepeticao
     // Exemplo: papagaio("Como estamos?", 3) => "Como estamos?Como estamos?Como estamos?"
+    return string.repeat(numRepeticao)
 }
 
 module.exports = {
